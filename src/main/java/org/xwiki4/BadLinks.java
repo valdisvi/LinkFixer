6c733ce0cb81f4095b1922269c59716c225484cf
Sabine Shuhrati <sabina.shuhrati@gmail.com>
2019-08-22 09:30:04 +0300

    Improved BadLinks class

--- src/main/java/org/xwiki4/BadLinks.java
+++ src/main/java/org/xwiki4/BadLinks.java
@@ -10,21 +10,28 @@ import org.jsoup.select.Elements;
 
 
 public class BadLinks {
+	
+	private ArrayList<String> parentLinks = new ArrayList<>();
+	private ArrayList<String> realLinks = new ArrayList<>();
 
-	public static void main(String[] args) throws IOException {
-        String url = "https://odo.lv/xwiki/bin/download/Challenges/WebHome/linkchecker.html";
-
+	public void findLinks (String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         Elements links = doc.select("a:nth-child(1)[target=top][href]");
-		ArrayList<String> parentLinks = new ArrayList<>();
-		ArrayList<String> realLinks = new ArrayList<>();
         for (Element link : links) {
         	if(links.indexOf(link)%2 != 0)
         		parentLinks.add(link.attr("abs:href"));
         	else
         		realLinks.add(link.attr("abs:href"));
         }
-        for(int i=0; i<parentLinks.size(); i++) {
+	}
+	public ArrayList<String> getParentLinks () {
+		return parentLinks;
+	}
+	public ArrayList<String> getRealLinks () {
+		return realLinks;
+	}
+	public void printLinks () {
+        for(int i=0; i<realLinks.size(); i++) {
         	System.out.println("Parent URL: " + parentLinks.get(i) + "  Real URL: " + realLinks.get(i));
         }
     }
