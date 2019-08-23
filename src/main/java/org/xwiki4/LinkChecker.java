87ee3e0481cc234edef63d96210c4ca7d4dc2011
Edmunds Ozolins <edmundsozolins125@gmail.com>
2019-08-23 12:43:20 +0300

    message

--- src/main/java/org/xwiki4/LinkChecker.java
+++ src/main/java/org/xwiki4/LinkChecker.java
@@ -5,9 +5,9 @@ public class LinkChecker {
 
     public static void main(final String[] args) throws IOException, InterruptedException {
     	
-        String[] cmd = {"linkchecker", "--no-warnings", "--ignore-url=^mailto --ignore-url=^http://localhost --ignore-url=^apt --ignore-url=.*?viewer=.*"
-        		, "--no-follow-url=.*\\?viewer=.*", "--check-extern", "-r6", "-ohtml", "-t5", "-Fhtml//home/student/workspace/XWiki/src/main/java/badlinks.html"
-        		, "http://192.168.8.113:8080/xwiki/bin/view/TestPageLinkChecker/SimplePage/"};
+        String[] cmd = {"linkchecker", "--no-warnings", "-p", "-usuperadmin", "--ignore-url=^mailto --ignore-url=^http://localhost --ignore-url=^apt --ignore-url=.*?viewer=.*"
+        		, "--no-follow-url=.*\\?viewer=.*", "--check-extern", "-r3", "-ohtml", "-t5", "-Fhtml//home/student/Team4_XWiki/XWiki/src/test/resources/badlinks.html"
+        		, "http://192.168.8.150:8080/xwiki/bin/view/TestPageLinkChecker/SimplePage/"};
         
         Utils.executeCmd(cmd);
     }
