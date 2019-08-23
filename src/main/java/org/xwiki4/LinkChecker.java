ef4b3e4bec073e62de322b4b2018250eafe13ac9
Sabine Shuhrati <sabina.shuhrati@gmail.com>
2019-08-23 11:57:53 +0300

    Remove main method

--- src/main/java/org/xwiki4/LinkChecker.java
+++ src/main/java/org/xwiki4/LinkChecker.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 
 public class LinkChecker {
 
-    public static void main(final String[] args) throws IOException, InterruptedException {
+    public static void check (String url) throws IOException, InterruptedException {
     	
         String[] cmd = {"linkchecker", "--no-warnings", "--ignore-url=^mailto --ignore-url=^http://localhost --ignore-url=^apt --ignore-url=.*?viewer=.*"
-        		, "--no-follow-url=.*\\?viewer=.*", "--check-extern", "-r6", "-ohtml", "-t5", "-Fhtml//home/student/workspace/XWiki/src/main/java/badlinks.html"
-        		, "http://192.168.8.113:8080/xwiki/bin/view/TestPageLinkChecker/SimplePage/"};
+        		, "--no-follow-url=.*\\?viewer=.*", "--check-extern", "-r1", "-ohtml", "-t5", "-Fhtml//home/student/workspace/XWiki/linkchecker.html"
+        		, url};
         
         Utils.executeCmd(cmd);
     }
