b8c0d38b15614568863d53af1d297a0fba212b17
Sabine Shuhrati <sabina.shuhrati@gmail.com>
2019-08-27 10:17:31 +0300

    BadLinks JUnit test

---
+++ src/test/java/org/xwiki4/test/BadLinksTest.java
@@ -0,0 +1,42 @@
+package org.xwiki4.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.xwiki4.BadLinks;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BadLinksTest {
+
+	@Test
+	public void test01FindLinks() {
+		BadLinks badLinks = new BadLinks();
+		try {
+			badLinks.findLinks("https://odo.lv/xwiki/bin/download/Challenges/WebHome/linkchecker.html");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals(26, badLinks.getErrorCount());
+		Assert.assertEquals(badLinks.getParentLinks().size(), badLinks.getRealLinks().size());
+	}
+	
+	@Test
+	public void test02FindLinksLocal() {
+		BadLinks badLinks = new BadLinks();
+		try {
+			File file = new File("src/test/resources/badlinks.html");
+			badLinks.findLinksLocal(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals(239, badLinks.getErrorCount());
+		Assert.assertEquals(badLinks.getParentLinks().size(), badLinks.getRealLinks().size());
+	}
+	
+
+}
