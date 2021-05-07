726e1b6f5877e71012d080f094095623ff5c0ee7
Valdis Vitolins <valdis.vitolins@odo.lv>
2021-05-07 12:02:31 +0300

    Organize imports

--- src/main/java/org/xwiki4/LinkFixer.java
+++ src/main/java/org/xwiki4/LinkFixer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.lang3.StringUtils;
 
 /*
  * @author Edmunds Ozolins
